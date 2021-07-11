a2de73b9c65ace39f3168ad0145246a7c087ef17
RafikBoubaker <boubaker.rafik@gmail.com>
2021-07-11 13:48:51 +0100

    API architecture Final

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -32,11 +32,29 @@ namespace API.Controllers
             return Ok(products);
         }
 
+
          [HttpGet("{id}")]
 
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
            return await _repo.GetProductByIdAsync(id);
+        }
+
+
+        [HttpGet("brands")]
+
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+           return Ok(await _repo.GetProductBrandsAsync()); 
+        }
+
+
+
+         [HttpGet("types")]
+
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+           return Ok(await _repo.GetProductTypesAsync()); 
         }
         
     }
--- Infrastructure/Data/Migrations/20210711102103_InitialCreate.Designer.cs
+++ Infrastructure/Data/Migrations/20210711102103_InitialCreate.Designer.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    [Migration("20210710205415_InitialCreate")]
+    [Migration("20210711102103_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -24,16 +24,20 @@ namespace Infrastructure.Data.Migrations
                         .HasColumnType("INTEGER");
 
                     b.Property<string>("Description")
+                        .IsRequired()
+                        .HasMaxLength(180)
                         .HasColumnType("TEXT");
 
                     b.Property<string>("Name")
+                        .IsRequired()
                         .HasColumnType("TEXT");
 
                     b.Property<string>("PictureUrl")
+                        .IsRequired()
                         .HasColumnType("TEXT");
 
                     b.Property<decimal>("Price")
-                        .HasColumnType("TEXT");
+                        .HasColumnType("decimal(18,2)");
 
                     b.Property<int>("ProductBrandId")
                         .HasColumnType("INTEGER");
