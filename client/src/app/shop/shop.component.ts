eb814d4c339a985661b13535ea7c8a933c8affde
RafikBoubaker <boubaker.rafik@gmail.com>
2021-08-13 17:12:57 +0100

    frontEnd update V0.4

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
+import { IProduct } from '../shared/models/product';
+import { IType } from '../shared/models/ProductType';
+import { ShopService } from './shop.service';
 
 @Component({
   selector: 'app-shop',
@@ -6,10 +10,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
-
-  constructor() { }
+products : IProduct[]
+brands : IBrand[]
+types:IType[]
+  constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
+
+    this.getProducts()
+    this.getBrand()
+    this.getTypes()
   }
+
+  
+      getProducts(){
+        this.shopService.getProducts().subscribe(response =>{
+          this.products = response.data
+        },err =>{
+          console.log(err)
+        })
+      }
+
+
+
+      getBrand(){
+
+        this.shopService.getBrands().subscribe(response =>{
+          this.brands = response
+        },err =>{
+          console.log(err)
+        })
+          
+      }
+
+
+      getTypes(){
+
+        this.shopService.getTypes().subscribe(response =>{
+          this.types = response
+        },err =>{
+          console.log(err)
+        })
+          
+      }
+
+
+
 
 }
